95d25496ae7651f46febfe06f446bf510ae6139e
kingsley <kingsleykay26@gmail.com>
2026-02-19 16:27:29 +0100

    wrapped todo input in a container

--- src/InputComponent.jsx
+++ src/InputComponent.jsx
@@ -4,14 +4,16 @@ export function InputComponent({ addToList, inputRef }) {
   return (
     <>
       <article className="input-section">
-        <input
-          ref={inputRef}
-          className="input-field"
-          placeholder="What's on your list today?"
-          onKeyDown={(e) => {
-            e.key === "Enter" && addToList();
-          }}
-        />
+        <div className="todo-input-container">
+          <input
+            ref={inputRef}
+            className="input-field"
+            placeholder="What's on your list today?"
+            onKeyDown={(e) => {
+              e.key === "Enter" && addToList();
+            }}
+          />
+        </div>
         <button className="add-btn" onClick={addToList}>
           Add <span className="optional-btn-text">Task</span>
         </button>
